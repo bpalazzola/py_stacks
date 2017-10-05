c7957553794d98cf8bfe9f65f0e11628fd459156
bpalazzola <bpalazzola@ymail.com>
2017-10-05 11:55:19 -0500

    port2

--- flask_fundamentals/Portfolio/portfolio.py
+++ flask_fundamentals/Portfolio/portfolio.py
@@ -3,5 +3,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/projects')
+def projects():
+    my_projects = ['Ninja Gold', 'Great Number Game',
+                   'Disappearing Ninjas', 'Dojo Survey']
+    return render_template('projects.html', projects=my_projects)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+app.run(debug=True)
